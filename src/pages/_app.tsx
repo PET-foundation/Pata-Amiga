96084544a485937d965f950d75ba282d824d7412
MatheusVict <matheusvictorhenrique@gmail.com>
2023-09-04 22:46:29 -0300

    feat: configure chakra
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { extendTheme } from '@chakra-ui/react'
+
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+export const theme = extendTheme({ colors })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={colors}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
